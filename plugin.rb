9fd4eb5704a8e5bb4bb31081487fbfaa00d4d67a
Arpit Jalan <arpit@techapj.com>
2015-12-02 02:47:10 +0530

    minor optimizations

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,6 @@ after_initialize do
   add_to_serializer(:current_user, :can_see_topic_group_button?) do
     return true if scope.is_staff?
     group = Group.find_by("lower(name) = ?", SiteSetting.topic_group_button_allowed_group.downcase)
-    group_user = GroupUser.find_by(user_id: scope.user.id, group_id: group.id)
-    return true if group_user && group_user.id
+    return true if group && GroupUser.where(user_id: scope.user.id, group_id: group.id).exists?
   end
 end

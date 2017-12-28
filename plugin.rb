114c7950021c89946f73cecfc1b1267f9294c49d
Richard <support@discoursehosting.com>
2017-12-28 17:28:55 +0000

    Bump version, add URL, add enabled switch

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,10 @@
 # name: discourse-topic-group-button
 # about: adds a custom button at the bottom of a topic, visible only to staff or members of a specific group
-# version: 0.1
+# version: 0.2
 # authors: Robin Ward, Arpit Jalan
+# url: https://github.com/discourse/discourse-topic-group-button
+
+enabled_site_setting :topic_group_button_enabled
 
 after_initialize do
   add_to_serializer(:current_user, :can_see_topic_group_button?) do
